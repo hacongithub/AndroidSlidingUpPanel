3db91e2fc530e8c7a32f709895000d0a4e74882a
Mark Dresselhaus <mdr@hacon.de>
2020-04-20 09:23:45 +0200

    Added missing imports

--- demo/src/main/java/de/hafas/slidinguppanel/demo/DemoActivity.java
+++ demo/src/main/java/de/hafas/slidinguppanel/demo/DemoActivity.java
@@ -3,6 +3,9 @@ package de.hafas.slidinguppanel.demo;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.text.Html;
